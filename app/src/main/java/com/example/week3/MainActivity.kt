b4aa3254970ac952cb97e3d35aea0966a0b28afd
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-10-18 13:33:08 +0700

    menambahkan tulisan halaman login

---
+++ app/src/main/java/com/example/week3/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.week3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.week3.ui.theme.Week3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Week3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicColumn(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicColumn(modifier: Modifier = Modifier){
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp)
+    )
+    {
+        Text("LOGIN",
+            fontSize = 100.sp,
+            fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.padding(18.dp))
+        Text("Ini Adalah Halaman Login",
+            fontSize = 50.sp,
+            fontStyle = FontStyle.Italic,
+            )
+        Spacer(modifier = Modifier.padding(20.dp))
+        Image(painter = painterResource(id = R.drawable.a),
+            contentDescription = "",
+            modifier = Modifier.clip(RectangleShape))
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Week3Theme {
+        BasicColumn()
+    }
+}
